950f2d2d95bf1184b72eeac5306fab2f049b2be5
GabrielLopes <gabrielfcb.2701@gmail.com>
2025-04-02 15:16:51 -0300

    i

--- java script/aulas/modulo-e/aula01/opivrsun.js
+++ java script/aulas/modulo-e/aula01/opivrsun.js
@@ -1,9 +1,26 @@
-/*var c = 1
-while (c <= 5) {
-    console.log(c)
-    c++
-}*/
+function Contar() {
 
-    for(var c = 1; c <= 5; c++) {
-        document.write(c)
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+        window.alert('coloca o bagulho ai tio, deixa em branco não')
+    } else {
+        res.innerHTML = 'contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        for(let c = i; c <= f; c += p) {
+        res.innerHTML += `${c}🤟🏽 &nbsp;`
     }
+    }
+
+    
+
+
+
+
+}
